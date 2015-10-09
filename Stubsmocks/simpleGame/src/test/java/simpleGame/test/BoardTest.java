2f4abc12abe48f4e08b7a0ef9647cd4fc2b75bf7
Maxime Lebastard <maxime.lebastard@etudiant.univ-rennes1.fr>
2015-10-09 19:32:57 +0200

    Add tests

--- Stubsmocks/simpleGame/src/test/java/simpleGame/test/BoardTest.java
+++ Stubsmocks/simpleGame/src/test/java/simpleGame/test/BoardTest.java
@@ -5,6 +5,7 @@ import static org.mockito.Mockito.*;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 //Eclipse might not find this one automatically:
 import org.mockito.runners.MockitoJUnitRunner;
 
@@ -16,15 +17,48 @@ import simpleGame.core.Pawn;
 public class BoardTest extends TestCase {
 
 	@Test
-	public void maxGold() {
-		Board board = new Board(1, 5, 5, 3, 3);
+	public void construct_correct() {
+		Pawn pawnMock = Mockito.mock(Pawn.class);
 		
-		Pawn mockPawn = mock(Pawn.class);
-		when(mockPawn.getGold()).thenReturn(3);
+		Board board = new Board(3, 5, 5, 2, 2);
+		assertEquals(3, board.numberOfPawns());
+	}
 	
-		board.addPawn(mockPawn);
+	@Test
+	public void addPawn_correct() {
+		Pawn pawnMock = Mockito.mock(Pawn.class);
 		
-		assertEquals((int) 3, board.maxGold());
+		Board board = new Board(1, 5, 5, 2, 2);
+		board.addPawn(pawnMock);
+	
+		assertEquals(2, board.numberOfPawns());
+	}
+	
+	@Test
+	public void removePawn_correct() {
+		Pawn pawnMock = Mockito.mock(Pawn.class);
+		
+		Board board = new Board(1, 5, 5, 2, 2);
+		board.addPawn(pawnMock);
+		board.removePawn(pawnMock);
+		assertEquals(1, board.numberOfPawns());
+	}
+	
+	@Test
+	public void removeAllPawn_correct() {
+		Pawn pawnMock = Mockito.mock(Pawn.class);
+		Pawn pawnMock2 = Mockito.mock(Pawn.class);
+		Pawn pawnMock3 = Mockito.mock(Pawn.class);
+
+		
+		Board board = new Board(1, 5, 5, 2, 2);
+		board.addPawn(pawnMock);
+		board.addPawn(pawnMock2);
+		assertEquals(2, board.numberOfPawns());
+		
+		board.removeAllPawns();
+		
+		assertEquals(0, board.numberOfPawns());
 	}
 
 }
---
+++ Stubsmocks/simpleGame/src/test/java/simpleGame/test/PawnTest.java
@@ -0,0 +1,53 @@
+package simpleGame.test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import static org.mockito.Mockito.*;
+
+import org.mockito.Mockito;
+//Eclipse might not find this one automatically:
+import org.mockito.runners.MockitoJUnitRunner;
+
+import junit.framework.TestCase;
+import simpleGame.core.Board;
+import simpleGame.core.Direction;
+import simpleGame.core.Pawn;
+import simpleGame.exception.OutOfBoardException;
+
+@RunWith(MockitoJUnitRunner.class) // This is required for mocks to work
+public class PawnTest extends TestCase {
+
+	@Test
+	public void construct_correct() {
+		Board boardMock = Mockito.mock(Board.class);
+		Pawn pawn = new Pawn('a',1,2,boardMock);
+
+        assertEquals('a', pawn.getLetter());
+        assertEquals(1, pawn.getX());
+        assertEquals(2, pawn.getY());
+		assertEquals(0, pawn.getGold());
+	}
+	
+	@Test
+	public void move_correct() throws OutOfBoardException {
+		Board boardMock = Mockito.mock(Board.class);
+		when(boardMock.getXSize()).thenReturn(5);
+		when(boardMock.getYSize()).thenReturn(5);
+		
+		Pawn pawn = new Pawn('a',1,2,boardMock);
+		pawn.move(Direction.Right);
+		
+		assertEquals(2, pawn.getX());
+	}
+	
+	@Test(expected = OutOfBoardException.class)
+	public void move_out() throws OutOfBoardException {
+		Board boardMock = Mockito.mock(Board.class);
+		when(boardMock.getXSize()).thenReturn(5);
+		when(boardMock.getYSize()).thenReturn(5);
+		
+		Pawn pawn = new Pawn('a',1,2,boardMock);
+		pawn.move(Direction.Left);
+	}
+
+}
